843dfdfe1e49b4f6d26e62355e2e94def460759c
jatnrai <raijatin62@gmail.com>
2024-02-18 01:00:11 +0530

    setUp commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reac
-        </a>
-      </header>
+      <div className="background-container">
+       <div className="overlay"></div>
+       <div className="text-center">
+
+       <h1 className="my-0">Good {"Afternoon"}</h1>
+       </div>
+      </div>
     </div>
   );
 }
